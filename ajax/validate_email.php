80e1d4ff51bdf9b029c64a9ab56ae1d8cac60855
Abnet Mekonen Gosaye <abnetmekonen32@gmail.com>
2026-05-15 19:46:42 +0300

    refactore:comment text

--- ajax/validate_email.php
+++ ajax/validate_email.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 require_once '../connection.php';
 
-// Normalize connection variable name from included 
+// Normalize connection variable name from included
 if (!isset($con) && isset($conn)) {
 	$con = $conn;
 }
