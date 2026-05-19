6aee94006c978c79f64e820419aa8201ac1e2bb5
Abnet Mekonen Gosaye <abnetmekonen32@gmail.com>
2026-05-19 20:10:54 +0300

    refactore:text content

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ require 'includes/header.php';
         <div class="auth-header">
             <div class="auth-icon"><i class="fas fa-store"></i></div>
             <h1 class="form-title">Welcome Back</h1>
-            <p class="form-subtitle">Sign in to yo</p>
+            <p class="form-subtitle">Sign in to y</p>
         </div>
 
         <?php if ($error): ?>
